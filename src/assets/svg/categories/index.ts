3b81311a62c14311d6594ce720bc42031c44bda1
Paulo Bahia <pauloipatinga22@gmail.com>
2025-01-17 15:47:16 -0300

    feat: Add multiple SVG icons for various categories.
    - Create a new SVG file for a calendar icon with specific dimensions and styling.
    - Add detailed clapper board SVG icon.
    - Add new SVG image for commercial category.
    - Add disc SVG category icon with colorful design.
    - Add flag SVG icon to the project.
    - Add SVG folder icon with custom design.
    - Add guitar SVG icon to assets.
    - Add new SVG categories exports.
    - Add keyboard music SVG icon.
    - Add detailed microphone SVG icon with different path styles.
    - Add new SVG icon for category "music note".
    - Add pending file SVG icon.
    - Add saxophone SVG icon to assets.
    - Add singer SVG icon from SVG Repo with detailed path information.
    - Add star SVG icon in category images.
    - Add SVG icon for text category.
    - Rename variables for SVG files in index.ts.

---
+++ src/assets/svg/categories/index.ts
@@ -0,0 +1,33 @@
+import CalendarSVG from './calendar.svg'
+import ClapperBoardSVG from './clapper-board.svg'
+import DiscSVG from './disc.svg'
+import FlagSVG from './flag.svg'
+import FolderSVG from './folder.svg'
+import GuitarSVG from './guitar.svg'
+import KeyboardMusicSVG from './keyboard-music.svg'
+import MicSVG from './mic.svg'
+import MusicNoteSVG from './music-note.svg'
+import SaxSVG from './sax.svg'
+import SingerSVG from './singer.svg'
+import StarSVG from './star.svg'
+import CommercialSVG from './commercial.svg'
+import TextSVG from './text.svg'
+import PendingFileSVG from './pending-file.svg'
+
+export {
+  CalendarSVG,
+  ClapperBoardSVG,
+  DiscSVG,
+  FlagSVG,
+  FolderSVG,
+  GuitarSVG,
+  KeyboardMusicSVG,
+  MicSVG,
+  MusicNoteSVG,
+  SaxSVG,
+  SingerSVG,
+  StarSVG,
+  CommercialSVG,
+  TextSVG,
+  PendingFileSVG
+}
--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,5 +1,5 @@
-import MP3 from '@/assets/svg/mp3.svg'
-import MP4 from '@/assets/svg/mp4.svg'
-import TXT from '@/assets/svg/txt.svg'
+import MP3SVG from '@/assets/svg/mp3.svg'
+import MP4SVG from '@/assets/svg/mp4.svg'
+import TXTSVG from '@/assets/svg/txt.svg'
 
-export { MP3 as MP3SVG, MP4 as MP4SVG, TXT as TXTSVG }
+export { MP3SVG, MP4SVG, TXTSVG }
